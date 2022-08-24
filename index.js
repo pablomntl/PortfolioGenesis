dec59f52ae6e6839fd3d4a780c7e20ee79e50b48
pablo mereles <pablomereles62@gmail.com>
2022-08-23 22:28:26 -0300

    te amoooo, vino vecna

---
+++ index.js
@@ -0,0 +1,33 @@
+//Javacript for the scroll indicator bar
+window.addEventListener("scroll", () => {
+    const indicatorBar = document.querySelector(".scroll-indicator-bar");
+
+    const pageScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const scrollValue = (pageScroll / height) * 100;
+
+    indicatorBar.style.width = scrollValue + "%";
+  });
+
+  //Responsive navigation menu toggle
+  const menuBtn = document.querySelector(".nav-menu-btn");
+  const closeBtn = document.querySelector(".nav-close-btn");
+  const navigation = document.querySelector(".navigation");
+
+  menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+  });
+
+  // const $contenidoMas = document.querySelector(".contenidoMas");
+  // const $botonPrueba = document.querySelector(".btn-home");
+  // $botonPrueba.addEventListener("click", () => {
+  //   $contenidoMas.classList.add("know-more");
+  //   $contenidoMas.innerHTML = `Bienvenidos esta es mi pagina`;
+  // });
